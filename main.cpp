00496a999c75f9d795e3fa6c4bb9aded68702af5
tracer <46232881+joker5743@users.noreply.github.com>
2022-10-26 15:04:21 +0800

    Revert "merge Sep to Oct practice"

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include"data_structure.cpp"
+#include"WidthOfBinaryTree.cpp"
+using namespace std;
+
+int main(){
+    cout << "Programm start..." << endl;
+    
+    WidthOfBinaryTree *wt = new WidthOfBinaryTree();
+
+    cout << "Programm finished..." << endl;
+    return 0;
+}
