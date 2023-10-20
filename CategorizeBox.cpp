740f4226b1eebd80a886ce5348769f8bf06bbe10
pengjunhan <pengjunhan@lixiang.com>
2023-10-20 18:39:17 +0800

    submit on Oct. 20th 2023

---
+++ CategorizeBox.cpp
@@ -0,0 +1,19 @@
+// 2525. 箱体分类
+using namespace std;
+
+class CategorizeBox {
+public:
+  string categorizeBox(int length, int width, int height, int mass) {
+    long long maxd = max(length, width, height),
+              vol = 1L * length * width * height;
+    bool isBulky = maxd >= 10000 || vol >= 1000000000, isHeavy = mass >= 100;
+    if (isHeavy && isBulky)
+      return "Both";
+    else if (isBulky)
+      return "Bulky";
+    else if (isHeavy)
+      return "Heavy";
+    else:
+    return "Neither";
+  }
+};
