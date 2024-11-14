7fddb8fe5a96ceebad3978e5fca0142c965c83e7
mechanism_revolution <pengjunhan@126.com>
2024-11-14 15:00:32 +0800

    feat[win] leetcode daily practice submit on 14th November 2024

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 int main() {
   CountQuadruplets ques = CountQuadruplets();
-  std::vector input = {1, 3, 2, 4, 5};
+  std::vector<int> input = {1, 3, 2, 4, 5};
   int ans = ques.countQuadruplets(input);
   std::cout << "the program is finished here !" << std::endl;
   return 0;
